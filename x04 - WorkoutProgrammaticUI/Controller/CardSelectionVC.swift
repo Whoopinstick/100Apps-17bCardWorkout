c151a0e069580e9f1effa794d66e89824a4d3169
Whoopinstick <k.m.paul68@gmail.com>
2020-11-22 14:52:03 -0500

    base programmatic UI setup - remove main reference from plist, remove main from build settings, add VC reference in delegate

--- x04 - WorkoutProgrammaticUI/Controller/CardSelectionVC.swift
+++ x04 - WorkoutProgrammaticUI/Controller/CardSelectionVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardSelectionVC.swift
 //  x04 - WorkoutProgrammaticUI
 //
 //  Created by Kevin Paul on 11/22/20.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardSelectionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        view.backgroundColor = .brown
 
+        
+    }
+    
 
 }
-
